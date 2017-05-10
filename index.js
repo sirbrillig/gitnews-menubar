a4cd160ef55ac85503ccfb63bf994f32d9a73a09
Payton Swick <payton@foolord.com>
2017-05-10 19:17:31 -0400

    Preload the window

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { ipcMain } = require( 'electron' );
 const menubar = require( 'menubar' );
-const bar = menubar();
-
-bar.on( 'after-create-window', () => {
-	//bar.window.openDevTools();
+const bar = menubar( {
+	preloadWindow: true,
 } );
 
 ipcMain.on( 'unread-notifications-count', ( event, arg ) => {
