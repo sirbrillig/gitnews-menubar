cda4c4196d0eddd4dacbeb1e9d49b8e39fe61997
Payton Swick <payton@foolord.com>
2017-05-09 21:18:28 -0400

    Replace rendering with React

--- app.js
+++ app.js
@@ -3,43 +3,55 @@ require( 'dotenv' ).config();
 const { getNotifications } = require( 'gitnews' );
 const Conf = require( 'conf' );
 const config = new Conf();
+const React = require( 'react' );
+const ReactDOM = require( 'react-dom' );
+const el = React.createElement;
 
 function getToken() {
 	return process.env.GITNEWS_TOKEN || config.get( 'gitnews-token' );
 }
 
-function bindToTarget( target, func ) {
-	return func.bind( null, target );
+function Notification( { note } ) {
+	return el( 'div', { className: 'notification' }, [
+		el( 'span', { className: 'notification__repo', key: 'notification__repo' }, note.repository.full_name ),
+		': ',
+		el( 'span', { className: 'notification__title', key: 'notification__title' }, note.subject.title ),
+	] );
 }
 
-function showNotification( target, notification ) {
-	console.log( 'showing notification', notification );
-	target.innerHTML = notification.subject.title;
+function NotificationsArea( { notes } ) {
+	const content = notes.length ? notes.map( note => el( Notification, { note, key: note.id } ) ) : 'No Notifications!';
+	return el( 'div', { className: 'notifications-area' }, content );
 }
 
-function showNoNotifications( target ) {
-	target.innerHTML = 'No Notifications!';
+class App extends React.Component {
+	constructor( props ) {
+		super( props );
+		this.state = { notes: [] };
+	}
+
+	componentDidMount() {
+		getNotifications( getToken() )
+			.then( notes => {
+				this.setState( { notes } );
+			} )
+			.catch( function( err ) {
+				console.error( err );
+			} );
+	}
+
+	render() {
+		return el( NotificationsArea, { notes: this.state.notes } );
+	}
 }
 
 function runApp() {
 	const main = window.document.querySelector( '.main' );
 	if ( ! main ) {
+		console.error( 'Could not find main element' );
 		return;
 	}
-	main.innerHTML = 'this is javascript';
-	const showNote = bindToTarget( main, showNotification );
-	const noNotes = bindToTarget( main, showNoNotifications );
-	getNotifications( getToken() )
-		.then( function( notifications ) {
-			if ( notifications.length < 1 ) {
-				noNotes();
-				return;
-			}
-			notifications.map( showNote );
-		} )
-		.catch( function( err ) {
-			main.innerHTML = 'this is an error: ' + err;
-		} );
+	ReactDOM.render( el( App ), main );
 }
 
 runApp();
