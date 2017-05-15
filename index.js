0f9397fbf21f8b7b257681b1bf4832072205223d
Payton Swick <payton@foolord.com>
2017-05-15 16:26:51 -0400

    Allow packaging app for Mac OS

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const { ipcMain } = require( 'electron' );
+const { ipcMain, nativeImage, app } = require( 'electron' );
+const path = require( 'path' );
 const menubar = require( 'menubar' );
+
+const unhandled = require( 'electron-unhandled' );
+
+// Catch unhandled Promise rejections
+unhandled();
+
+const appDir = app.getAppPath();
+const alertIconPath = path.join( appDir, 'IconTemplateAlert.png' );
+const normalIconPath = path.join( appDir, 'IconTemplate.png' );
+const alertIcon = nativeImage.createFromPath( alertIconPath );
+const normalIcon = nativeImage.createFromPath( normalIconPath );
+
+// Create menubar
 const bar = menubar( {
 	preloadWindow: true,
 } );
 
 ipcMain.on( 'unread-notifications-count', ( event, arg ) => {
-	const image = arg > 0 ? './IconTemplateAlert.png' : './IconTemplate.png';
+	const image = arg > 0 ? alertIcon : normalIcon;
 	bar.tray.setImage( image );
 } );
