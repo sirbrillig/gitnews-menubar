71277fec0a0f6e484719e0182756023f8257c06f
Payton Swick <payton@foolord.com>
2017-05-17 16:52:31 -0400

    Modify notification design to have icon,time,dot

--- app.js
+++ app.js
@@ -23,16 +23,30 @@ function setToken( token ) {
 	config.set( 'gitnews-token', token );
 }
 
+function UnreadDot() {
+	return el(
+		'svg',
+		{ width: '10', height: '10' },
+		el( 'circle', { cx: '5', cy: '5', r: '5', fill: 'orange' } )
+	);
+}
+
 function Notification( { note, openUrl, markRead } ) {
 	const onClick = () => {
 		debug( 'clicked on notification', note );
 		markRead( note );
 		openUrl( note.html_url );
 	};
+	const timeString = date.distanceInWords( Date.now(), date.parse( note.updated_at ), { addSuffix: true } );
 	const noteClass = note.unread ? ' notification__unread' : ' notification__read';
 	return el( 'div', { className: 'notification' + noteClass, onClick },
-		el( 'span', { className: 'notification__repo' }, note.repository.full_name + ': ' ),
-		el( 'span', { className: 'notification__title' }, note.subject.title )
+		el( 'div', { className: 'notification__dot' }, note.unread ? el( UnreadDot ) : null ),
+		el( 'div', { className: 'notification__image' }, el( 'img', { src: note.repository.owner.avatar_url } ) ),
+		el( 'div', { className: 'notification__body' },
+			el( 'div', { className: 'notification__repo' }, note.repository.full_name ),
+			el( 'div', { className: 'notification__title' }, note.subject.title ),
+			el( 'div', { className: 'notification__time' }, timeString )
+		)
 	);
 }
 
