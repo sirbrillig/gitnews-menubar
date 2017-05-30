486cb07996050eb542f2532dd9ae0e168946ff7a
Payton Swick <payton@foolord.com>
2017-05-30 17:04:55 -0400

    Use createUpdater HOC for LastChecked

--- components/header.js
+++ components/header.js
@@ -4,8 +4,10 @@ const Gridicon = require( 'gridicons' );
 const Logo = require( '../components/logo' );
 const LastChecked = require( '../components/last-checked' );
 const OfflineNotice = require( '../components/offline-notice' );
+const createUpdater = require( '../components/updater' );
+const UpdatingLastChecked = createUpdater( LastChecked );
 
-function Header( { openUrl, lastChecked, showConfig, offline, fetchNotifications, quitApp } ) {
+function Header( { openUrl, lastChecked, showConfig, offline, fetchNotifications, quitApp, getSecondsUntilNextFetch } ) {
 	const openLink = ( event ) => {
 		event.preventDefault();
 		openUrl( event.target.href );
@@ -17,7 +19,7 @@ function Header( { openUrl, lastChecked, showConfig, offline, fetchNotifications
 			showConfig ? el( 'a', { className: 'config-button', onClick: showConfig, href: '#', title: 'Configuration' }, el( Gridicon, { icon: 'cog' } ) ) : el( 'span', { className: 'config-spacer' } )
 		),
 		el( 'div', { className: 'header__secondary' },
-			lastChecked && el( LastChecked, { lastChecked } )
+			lastChecked && el( UpdatingLastChecked, { lastChecked, getSecondsUntilNextFetch } )
 		),
 		offline && el( OfflineNotice, { fetchNotifications } )
 	);
--- components/last-checked.js
+++ components/last-checked.js
@@ -1,49 +1,13 @@
-/* globals window */
 const React = require( 'react' );
 const el = React.createElement;
 const date = require( 'date-fns' );
-const debugFactory = require( 'debug' );
-const debug = debugFactory( 'gitnews-menubar' );
 
-class LastChecked extends React.Component {
-	constructor( props ) {
-		super( props );
-		this.lastCheckedUpdater = null;
-		this.updateInterval = 30000; // 30 seconds in ms
-		this.updateTimestamp = this.updateTimestamp.bind( this );
-		this.state = { lastUpdated: 0 };
+function LastChecked( { lastChecked } ) {
+	if ( ! lastChecked ) {
+		return null;
 	}
-
-	componentDidMount() {
-		if ( this.lastCheckedUpdater ) {
-			window.clearInterval( this.lastCheckedUpdater );
-		}
-		this.lastCheckedUpdater = window.setInterval( () => this.updateTimestamp(), this.updateInterval );
-	}
-
-	componentWillUnmount() {
-		if ( this.lastCheckedUpdater ) {
-			window.clearInterval( this.lastCheckedUpdater );
-		}
-	}
-
-	updateTimestamp() {
-		debug( 'updating LastChecked timestamp', this.state );
-		// Just a hack to force re-rendering
-		this.setState( { lastUpdated: Date.now() } );
-	}
-
-	render() {
-		const lastChecked = this.props.lastChecked;
-		if ( ! lastChecked ) {
-			return null;
-		}
-		const lastCheckedString = date.distanceInWords( Date.now(), date.parse( lastChecked ), { addSuffix: true } );
-		debug( 'updating LastChecked display for', lastChecked, lastCheckedString );
-		return el( 'div', { className: 'last-checked' },
-			'last checked: ' + lastCheckedString
-		);
-	}
+	const lastCheckedString = date.distanceInWords( Date.now(), date.parse( lastChecked ), { addSuffix: true } );
+	return el( 'div', { className: 'last-checked' }, 'last checked: ' + lastCheckedString );
 }
 
 module.exports = LastChecked;
