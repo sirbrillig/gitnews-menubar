646bc6750eb643f5a6f96cc04cb90cdd17f00fc6
Payton Swick <payton@foolord.com>
2017-05-31 13:55:24 -0400

    Add tests for NOTES_RETRIEVED

--- tests/reducer.js
+++ tests/reducer.js
@@ -35,4 +35,62 @@ describe( 'reducer', function() {
 			expect( result.notes[ 2 ].unread ).to.be.true;
 		} );
 	} );
+
+	describe( 'NOTES_RETRIEVED', function() {
+		const notes = [
+			{ id: 'a1', unread: true, title: 'test note 1' },
+			{ id: 'a2', unread: false, title: 'test note 2' },
+			{ id: 'a3', unread: true, title: 'test note 3' },
+		];
+
+		it( 'disables offline mode', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [], offline: true }, action );
+			expect( result.offline ).to.be.false;
+		} );
+
+		it( 'sets lastChecked date', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [] }, action );
+			expect( result.lastChecked ).to.exist;
+		} );
+
+		it( 'sets lastSuccessfulCheck date', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [] }, action );
+			expect( result.lastSuccessfulCheck ).to.exist;
+		} );
+
+		it( 'resets fetchRetryCount to 0', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [], fetchRetryCount: 11 }, action );
+			expect( result.fetchRetryCount ).to.equal( 0 );
+		} );
+
+		it( 'clears errors', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [], errors: [ 'a1', 'a2' ] }, action );
+			expect( result.errors ).to.be.empty;
+		} );
+
+		it( 'changes fetchInterval to the default', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [], fetchInterval: 9999 }, action );
+			expect( result.fetchInterval ).to.not.equal( 9999 );
+		} );
+
+		it( 'saves new notifications', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [] }, action );
+			expect( result.notes ).to.have.length( 3 );
+		} );
+
+		it( 'removes notifications not in new data', function() {
+			const action = { type: 'NOTES_RETRIEVED', notes };
+			const result = reducer( { notes: [ { id: 'o1', title: 'test note' } ] }, action );
+			expect( result.notes.map( note => note.id ) ).to.not.include( 'o1' );
+		} );
+
+		it( 'preserves `seen` state for existing notifications' );
+	} );
 } );
