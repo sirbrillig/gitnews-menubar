f23d1499f54130d43c61cc25bc524d5b75f1be2d
Payton Swick <payton@foolord.com>
2017-05-31 11:08:31 -0400

    Move state manipulation to reducer

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ const el = React.createElement;
 const debugFactory = require( 'debug' );
 const debug = debugFactory( 'gitnews-menubar' );
 const unhandled = require( 'electron-unhandled' );
-const { getNoteId, getToken, setToken, mergeNotifications, msToSecs, secsToMs } = require( './lib/helpers' );
-const { PANE_NOTIFICATIONS, PANE_CONFIG, PANE_TOKEN } = require( './lib/constants' );
+const { getToken, setToken, msToSecs, isOfflineCode } = require( './lib/helpers' );
 const App = require( './components/app' );
-
-const defaultFetchInterval = secsToMs( 120 );
+const State = require( './lib/state' );
+const {
+	reducer,
+	changeToken,
+	changeToOffline,
+	gotNotes,
+	addConnectionError,
+	showEditToken,
+	hideEditToken,
+	hideConfig,
+	showConfig,
+	clearErrors,
+	markAllNotesSeen,
+	markRead,
+} = require( './lib/reducer' );
 
 // Catch unhandled Promise rejections
 unhandled();
@@ -20,43 +32,21 @@ unhandled();
 class AppState extends React.Component {
 	constructor( props ) {
 		super( props );
-		this.state = {
-			token: props.getToken(),
-			notes: [],
-			errors: [],
-			lastChecked: false,
-			lastSuccessfulCheck: false,
-			fetchInterval: defaultFetchInterval,
-			offline: false,
-			currentPane: PANE_NOTIFICATIONS,
-		};
+		const state = new State( reducer );
+		// TODO: remove callback when unmounting
+		state.addCallback( () => this.setState( state.getState() ) );
+		this.dispatch = state.dispatch;
+		this.state = {};
 
-		this.hideEditToken = this.hideEditToken.bind( this );
-		this.showEditToken = this.showEditToken.bind( this );
-		this.hideConfig = this.hideConfig.bind( this );
-		this.showConfig = this.showConfig.bind( this );
-		this.markRead = this.markRead.bind( this );
 		this.openUrl = this.openUrl.bind( this );
-		this.clearErrors = this.clearErrors.bind( this );
 		this.fetchNotifications = this.fetchNotifications.bind( this );
 		this.writeToken = this.writeToken.bind( this );
-		this.markAllNotesSeen = this.markAllNotesSeen.bind( this );
 		this.getSecondsUntilNextFetch = this.getSecondsUntilNextFetch.bind( this );
-	}
-
-	markAllNotesSeen() {
-		const notes = this.state.notes.map( note => {
-			note.gitnewsSeen = true;
-			return note;
-		} );
-		debug( 'marking all notes as seen', notes );
-		this.setState( { notes } );
 	}
 
 	writeToken( token ) {
 		setToken( token );
-		this.clearErrors();
-		this.setState( { token } );
+		this.dispatch( changeToken( token ) );
 		this.fetchNotifications( token );
 	}
 
@@ -74,82 +64,53 @@ class AppState extends React.Component {
 	fetchNotifications( token = null ) {
 		if ( ! window.navigator.onLine ) {
 			debug( 'skipping notifications check because we are offline' );
-			this.setState( { offline: true, lastChecked: Date.now(), fetchInterval: secsToMs( 30 ) } );
+			this.dispatch( changeToOffline() );
 			return;
 		}
 		debug( 'fetching notifications' );
 		this.props.getNotifications( token || this.state.token )
 			.then( notes => {
 				debug( 'notifications retrieved', notes );
-				debug( 'previous notifications', this.state.notes );
-				const mergedNotes = mergeNotifications( this.state.notes, notes );
-				debug( 'merged notifications', mergedNotes );
-				this.setState( { notes: mergedNotes, offline: false, lastChecked: Date.now(), lastSuccessfulCheck: Date.now(), fetchInterval: defaultFetchInterval } );
+				this.dispatch( gotNotes( notes ) );
 			} )
 			.catch( err => {
 				debug( 'fetching notifications failed with the error', err );
 				if ( err.code === 'GitHubTokenNotFound' ) {
 					return; // This is handled in render
 				}
-				if ( err.code === 'ENETDOWN' ) {
-					debug( 'notifications check failed because the network is down' );
-					this.setState( { offline: true, lastChecked: Date.now(), fetchInterval: secsToMs( 30 ) } );
-					return;
-				}
-				if ( err.code === 'ENOTFOUND' ) {
+				if ( isOfflineCode( err.code ) ) {
 					debug( 'notifications check failed because we are offline' );
-					this.setState( { offline: true, lastChecked: Date.now(), fetchInterval: secsToMs( 30 ) } );
-					return;
-				}
-				if ( err.code === 'ETIMEDOUT' || err.code === 'ECONNABORTED' ) {
-					debug( 'notifications check failed because of a timeout' );
-					this.setState( { offline: true, lastChecked: Date.now(), fetchInterval: secsToMs( 30 ) } );
+					this.dispatch( changeToOffline() );
 					return;
 				}
 				const errorString = 'Error fetching notifications: ' + err;
 				console.error( errorString );
-				this.setState( { errors: [ ...this.state.errors, errorString ], lastChecked: Date.now() } );
+				this.dispatch( addConnectionError( errorString ) );
 			} );
-	}
-
-	clearErrors() {
-		debug( 'clearing errors' );
-		this.setState( { errors: [] } );
 	}
 
 	openUrl( url ) {
 		shell.openExternal( url );
 	}
 
-	markRead( readNote ) {
-		const notes = this.state.notes.map( note => {
-			if ( getNoteId( note ) === getNoteId( readNote ) ) {
-				note.unread = false;
-			}
-			return note;
-		} );
-		this.setState( { notes } );
-	}
-
-	showConfig() {
-		this.setState( { currentPane: PANE_CONFIG } );
-	}
-
-	hideConfig() {
-		this.setState( { currentPane: PANE_NOTIFICATIONS } );
-	}
-
-	showEditToken() {
-		this.setState( { currentPane: PANE_TOKEN } );
-	}
-
-	hideEditToken() {
-		this.setState( { currentPane: PANE_CONFIG } );
+	bindToDispatch( action ) {
+		return ( ...args ) => this.dispatch( action( args ) );
 	}
 
 	getActions() {
-		const { hideEditToken, showEditToken, hideConfig, showConfig, markRead, openUrl, clearErrors, fetchNotifications, writeToken, markAllNotesSeen, getSecondsUntilNextFetch } = this;
-		return { hideEditToken, showEditToken, hideConfig, showConfig, markRead, openUrl, clearErrors, fetchNotifications, writeToken, markAllNotesSeen, getSecondsUntilNextFetch };
+		return {
+			hideEditToken: this.bindToDispatch( hideEditToken ),
+			showEditToken: this.bindToDispatch( showEditToken ),
+			hideConfig: this.bindToDispatch( hideConfig ),
+			showConfig: this.bindToDispatch( showConfig ),
+			markRead: this.bindToDispatch( markRead ),
+			clearErrors: this.bindToDispatch( clearErrors ),
+			markAllNotesSeen: this.bindToDispatch( markAllNotesSeen ),
+			openUrl: this.openUrl,
+			fetchNotifications: this.fetchNotifications,
+			writeToken: this.writeToken,
+			getSecondsUntilNextFetch: this.getSecondsUntilNextFetch,
+		};
 	}
 
 	render() {
