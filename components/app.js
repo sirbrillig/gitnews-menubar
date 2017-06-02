99f07250758bb75c6f0e000d137e5f139a8aba4d
Payton Swick <payton@foolord.com>
2017-06-02 18:44:24 -0400

    Wrap main pane area with CSSTransitionGroup

--- components/app.js
+++ components/app.js
@@ -4,13 +4,14 @@ const el = React.createElement;
 const { ipcRenderer } = require( 'electron' );
 const debugFactory = require( 'debug' );
 const debug = debugFactory( 'gitnews-menubar' );
+const CSSTransitionGroup = require( 'react-transition-group/CSSTransitionGroup' );
 const ConfigPage = require( '../components/config-page' );
 const UncheckedNotice = require( '../components/unchecked-notice' );
 const Header = require( '../components/header' );
 const ErrorsArea = require( '../components/errors-area' );
 const AddTokenForm = require( '../components/add-token-form' );
 const NotificationsArea = require( '../components/notifications-area' );
-const { PANE_CONFIG, PANE_TOKEN } = require( '../lib/constants' );
+const { PANE_CONFIG, PANE_TOKEN, PANE_NOTIFICATIONS } = require( '../lib/constants' );
 const { secsToMs } = require( '../lib/helpers' );
 
 class App extends React.Component {
@@ -55,40 +56,47 @@ class App extends React.Component {
 
 	render() {
 		const { offline, errors, currentPane, token, lastSuccessfulCheck, version } = this.props;
+		// These are actions
 		const { openUrl, clearErrors, hideConfig, showConfig, writeToken, markRead, showEditToken, hideEditToken, quitApp, getSecondsUntilNextFetch } = this.props;
 		// We have to have a closure because otherwise it will treat the event param as a token.
-		const fetchNotifications = () => {
-			debug( 'fetchNotifications called manually' );
-			this.props.fetchNotifications();
-		};
-		if ( ! token || currentPane === PANE_TOKEN ) {
-			return el( 'main', null,
-				el( Header, { offline, fetchNotifications, openUrl, quitApp, getSecondsUntilNextFetch } ),
-				el( ErrorsArea, { errors, clearErrors } ),
-				el( AddTokenForm, { token, openUrl, writeToken, hideEditToken, showCancel: currentPane === PANE_TOKEN } )
-			);
-		}
-		if ( currentPane === PANE_CONFIG ) {
-			return el( 'main', null,
-				el( Header, { offline, fetchNotifications, openUrl, quitApp, getSecondsUntilNextFetch } ),
-				el( ConfigPage, { openUrl, hideConfig, showEditToken, version } )
-			);
-		}
-		if ( ! lastSuccessfulCheck ) {
-			return el( 'main', null,
-				el( Header, { offline, fetchNotifications, openUrl, quitApp, getSecondsUntilNextFetch } ),
-				el( ErrorsArea, { errors, clearErrors } ),
-				el( UncheckedNotice )
-			);
-		}
+		const fetchNotifications = () => this.props.fetchNotifications();
+
 		const newNotes = this.getUnreadNotifications();
 		const readNotes = this.getReadNotifications();
 		const unseenNotes = this.getUnseenNotifications();
+
 		ipcRenderer.send( 'unread-notifications-count', errors.length || unseenNotes.length );
+
 		return el( 'main', null,
-			el( Header, { offline, fetchNotifications, openUrl, lastSuccessfulCheck, showConfig, quitApp, getSecondsUntilNextFetch } ),
+			el( Header,
+				{
+					offline,
+					fetchNotifications,
+					openUrl,
+					lastSuccessfulCheck: currentPane === PANE_NOTIFICATIONS && lastSuccessfulCheck,
+					showConfig,
+					quitApp,
+					getSecondsUntilNextFetch
+				} ),
 			el( ErrorsArea, { errors, clearErrors } ),
-			el( NotificationsArea, { newNotes, readNotes, markRead, openUrl } )
+			el( CSSTransitionGroup, {
+				// TODO: I need CSS for this
+				transitionName: 'pane',
+				transitionAppear: true,
+				transitionEnter: true,
+				transitionLeave: true,
+				transitionAppearTimeout: 800,
+				transitionEnterTimeout: 800,
+				transitionLeaveTimeout: 800,
+			},
+				// TODO: these each need keys
+				! token && el( AddTokenForm, { token, openUrl, writeToken, hideEditToken } ),
+				currentPane === PANE_TOKEN && el( AddTokenForm, { token, openUrl, writeToken, hideEditToken, showCancel: true } ),
+				currentPane === PANE_CONFIG && el( ConfigPage, { openUrl, hideConfig, showEditToken, version } ),
+				lastSuccessfulCheck || el( UncheckedNotice ),
+				// TODO: for some reason the NotificationsArea is invisible; possibly because of scrolling/position styles?
+				currentPane === PANE_NOTIFICATIONS && el( NotificationsArea, { newNotes, readNotes, markRead, openUrl } )
+			)
 		);
 	}
 }
