a002b683f466625abb5ae1ce1ca4ba8197e4e539
Payton Swick <payton@foolord.com>
2017-06-05 12:07:18 -0400

    Add an additional hyphen before version

--- components/copyright.js
+++ components/copyright.js
@@ -9,6 +9,7 @@ function Copyright( { openUrl, version } ) {
 	return el( 'div', { className: 'copyright' },
 		el( 'div', { className: 'copyright__text' },
 			el( 'a', { href: 'https://github.com/sirbrillig/gitnews-menubar', onClick: openLink }, 'gitnews-menubar' ),
+			' - ',
 			el( 'span', null, ` v${ version }` ),
 			' - ',
 			el( 'span', null, 'copyright 2017 Payton Swick' )
