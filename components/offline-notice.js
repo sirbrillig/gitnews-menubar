f2c5ff2297d77da620bd157523f24854a09a2a18
Payton Swick <payton@foolord.com>
2017-06-05 12:30:35 -0400

    Add 'Retrying now' to OfflineNotice

--- components/offline-notice.js
+++ components/offline-notice.js
@@ -2,8 +2,14 @@ const React = require( 'react' );
 const el = React.createElement;
 
 function OfflineNotice( { fetchNotifications, getSecondsUntilNextFetch } ) {
+	const secondsRemaining = getSecondsUntilNextFetch();
+	if ( secondsRemaining < 1 ) {
+		return el( 'div', { className: 'offline-notice' },
+			el( 'span', null, 'I\'m having trouble connecting. Retrying now...' )
+		);
+	}
 	return el( 'div', { className: 'offline-notice' },
-		el( 'span', null, `I'm having trouble connecting. Retrying in ${ getSecondsUntilNextFetch() } seconds. ` ),
+		el( 'span', null, `I'm having trouble connecting. Retrying in ${ secondsRemaining } seconds. ` ),
 		el( 'a', { href: '#', onClick: fetchNotifications }, 'Retry now' )
 	);
 }
