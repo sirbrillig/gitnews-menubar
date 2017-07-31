2352f15146c52a75a92aa53397a001ae1cdf05ed
Payton Swick <payton@foolord.com>
2017-07-31 13:00:14 -0400

    Make sure ErrorsArea has unique keys per error

--- components/errors-area.js
+++ components/errors-area.js
@@ -1,5 +1,6 @@
 const React = require( 'react' );
 const el = React.createElement;
+const { getErrorMessage } = require( '../lib/helpers' );
 
 function ClearErrorsButton( { clearErrors } ) {
 	return el( 'button', { className: 'clear-errors-button btn', onClick: clearErrors }, 'Clear Errors' );
@@ -12,7 +13,7 @@ function ErrorMessage( { error } ) {
 function ErrorsArea( { errors, clearErrors } ) {
 	return el( 'div', { className: 'errors-area' },
 		errors.length > 0 ? el( ClearErrorsButton, { clearErrors } ) : null,
-		errors.map( error => el( ErrorMessage, { error, key: error } ) )
+		errors.map( error => el( ErrorMessage, { error, key: getErrorMessage( error ) + Date.now() } ) )
 	);
 }
 
