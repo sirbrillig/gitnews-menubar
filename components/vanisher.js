c68a3d3887fb3205fa3e249f3855c5efad742444
Payton Swick <payton@foolord.com>
2017-09-08 10:43:08 -0400

    Adjust vanisher class

--- components/vanisher.js
+++ components/vanisher.js
@@ -2,7 +2,7 @@ const React = require( 'react' );
 const el = React.createElement;
 
 function Vanisher( { isVisible, children } ) {
-	const className = isVisible ? 'vanisher--visible' : 'vanisher--invisible';
+	const className = isVisible ? 'vanisher vanisher--visible' : 'vanisher vanisher--invisible';
 	return el( 'div', { className }, children );
 }
 
