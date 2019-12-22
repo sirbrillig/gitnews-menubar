023b6b56115dba27614734e0372d7da899288cf3
Payton Swick <payton@foolord.com>
2019-12-21 19:15:47 -0500

    Use JSX in createUpdater

--- src/renderer/components/updater.js
+++ src/renderer/components/updater.js
@@ -1,38 +1,43 @@
-import React from 'react' ;
-import { secsToMs } from 'common/lib/helpers' ;
+import React from 'react';
+import { secsToMs } from 'common/lib/helpers';
 
-const el = React.createElement;
-
-export default function createUpdater( WrappedComponent ) {
+export default function createUpdater(WrappedComponent) {
 	return class Updater extends React.Component {
-		constructor( props ) {
-			super( props );
+		constructor(props) {
+			super(props);
 			this.lastCheckedUpdater = null;
-			this.updateInterval = secsToMs( 1 );
-			this.updateTimestamp = this.updateTimestamp.bind( this );
+			this.updateInterval = secsToMs(1);
+			this.updateTimestamp = this.updateTimestamp.bind(this);
 			this.state = { lastUpdated: 0 };
 		}
 
 		componentDidMount() {
-			if ( this.lastCheckedUpdater ) {
-				window.clearInterval( this.lastCheckedUpdater );
+			if (this.lastCheckedUpdater) {
+				window.clearInterval(this.lastCheckedUpdater);
 			}
-			this.lastCheckedUpdater = window.setInterval( () => this.updateTimestamp(), this.updateInterval );
+			this.lastCheckedUpdater = window.setInterval(
+				() => this.updateTimestamp(),
+				this.updateInterval
+			);
 		}
 
 		componentWillUnmount() {
-			if ( this.lastCheckedUpdater ) {
-				window.clearInterval( this.lastCheckedUpdater );
+			if (this.lastCheckedUpdater) {
+				window.clearInterval(this.lastCheckedUpdater);
 			}
 		}
 
 		updateTimestamp() {
 			// Just a hack to force re-rendering
-			this.setState( { lastUpdated: Date.now() } );
+			this.setState({ lastUpdated: Date.now() });
 		}
 
 		render() {
-			return el( WrappedComponent, Object.assign( { componentLastUpdated: this.state.lastUpdated }, this.props ) );
+			const mergedProps = {
+				componentLastUpdated: this.state.lastUpdated,
+				...this.props,
+			};
+			return <WrappedComponent {...mergedProps} />;
 		}
 	};
 }
