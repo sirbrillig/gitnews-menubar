7410731590138d2f885c84a73559417b51903884
Payton Swick <payton@foolord.com>
2019-12-23 14:45:50 -0500

    Use JSX for NotificationsArea

--- src/renderer/components/notifications-area.js
+++ src/renderer/components/notifications-area.js
@@ -1,28 +1,64 @@
-import React from 'react' ;
-import Gridicon from 'gridicons' ;
-import Notification from '../components/notification' ;
-import { getNoteId } from 'common/lib/helpers' ;
-
-const el = React.createElement;
+import React from 'react';
+import Gridicon from 'gridicons';
+import Notification from '../components/notification';
+import { getNoteId } from 'common/lib/helpers';
 
 function NoNotificationsIcon() {
-	return el( Gridicon, { icon: 'checkmark-circle', size: 36, className: 'no-notifications-icon' } );
-}
-
-function NoNotifications() {
-	return el( 'div', { className: 'no-notifications' },
-		el( 'div', null,
-			el( NoNotificationsIcon ),
-			'No new notifications!'
-		)
+	return (
+		<Gridicon
+			icon="checkmark-circle"
+			size={36}
+			className="no-notifications-icon"
+		/>
 	);
 }
 
-export default function NotificationsArea( { newNotes, readNotes, markRead, markUnread, openUrl, token } ) {
-	const noteRows = newNotes.length ? newNotes.map( note => el( Notification, { note, key: getNoteId( note ), markRead, markUnread, token, openUrl } ) ) : el( NoNotifications );
-	const readNoteRows = readNotes.map( note => el( Notification, { note, key: getNoteId( note ), markRead, markUnread, token, openUrl } ) );
-	return el( 'div', { className: 'notifications-area' },
-		noteRows,
-		readNoteRows
+function NoNotifications() {
+	return (
+		<div className="no-notifications">
+			<div>
+				<NoNotificationsIcon>No new notifications!</NoNotificationsIcon>
+			</div>
+		</div>
+	);
+}
+
+export default function NotificationsArea({
+	newNotes,
+	readNotes,
+	markRead,
+	markUnread,
+	openUrl,
+	token,
+}) {
+	const noteRows = newNotes.length ? (
+		newNotes.map(note => (
+			<Notification
+				note={note}
+				key={getNoteId(note)}
+				markRead={markRead}
+				markUnread={markUnread}
+				token={token}
+				openUrl={openUrl}
+			/>
+		))
+	) : (
+		<NoNotifications />
+	);
+	const readNoteRows = readNotes.map(note => (
+		<Notification
+			note={note}
+			key={getNoteId(note)}
+			markRead={markRead}
+			markUnread={markUnread}
+			token={token}
+			openUrl={openUrl}
+		/>
+	));
+	return (
+		<div className="notifications-area">
+			{noteRows}
+			{readNoteRows}
+		</div>
 	);
 }
