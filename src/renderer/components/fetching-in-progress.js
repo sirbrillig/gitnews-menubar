3a70333c907c45021813509b7e7992af9cac3a53
Payton Swick <payton@foolord.com>
2019-12-23 14:26:12 -0500

    Fix className of FetchingInProgress

--- src/renderer/components/fetching-in-progress.js
+++ src/renderer/components/fetching-in-progress.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function FetchingInProgress() {
 	return (
-		<div className="fething-in-progress">
+		<div className="fetching-in-progress">
 			<span>fetching notifications...</span>
 		</div>
 	);
