36447d5396234097ade8c1281478f524889869a8
Payton Swick <payton@foolord.com>
2019-12-23 14:38:51 -0500

    Use JSX for Notification

--- src/renderer/components/notification.js
+++ src/renderer/components/notification.js
@@ -1,62 +1,107 @@
-import React from 'react' ;
-import Gridicon from 'gridicons' ;
-import debugFactory from 'debug' ;
-import date from 'date-fns' ;
-import EnsuredImage from '@sirbrillig/ensured-image' ;
+import React from 'react';
+import Gridicon from 'gridicons';
+import debugFactory from 'debug';
+import date from 'date-fns';
+import EnsuredImage from '@sirbrillig/ensured-image';
 
-const el = React.createElement;
-const debug = debugFactory( 'gitnews-menubar' );
+const debug = debugFactory('gitnews-menubar');
 
-export default function Notification( { note, openUrl, markRead, markUnread, token } ) {
-	const isUnread = note.unread === true ? true : note.gitnewsMarkedUnread === true;
+export default function Notification({
+	note,
+	openUrl,
+	markRead,
+	markUnread,
+	token,
+}) {
+	const isUnread =
+		note.unread === true ? true : note.gitnewsMarkedUnread === true;
 	const onClick = () => {
-		debug( 'clicked on notification', note );
-		markRead( token, note );
-		openUrl( note.commentUrl );
+		debug('clicked on notification', note);
+		markRead(token, note);
+		openUrl(note.commentUrl);
 	};
-	const timeString = date.distanceInWords( Date.now(), date.parse( note.updatedAt ), { addSuffix: true } );
+	const timeString = date.distanceInWords(
+		Date.now(),
+		date.parse(note.updatedAt),
+		{ addSuffix: true }
+	);
 	const noteClass = isUnread ? ' notification__unread' : ' notification__read';
-	const defaultAvatar = `https://avatars.io/twitter/${ note.repositoryFullName }`;
-	const avatarSrc = note.commentAvatar || note.repositoryOwnerAvatar || defaultAvatar;
-	const isClosed = note.api.subject && note.api.subject.state && note.api.subject.state === 'closed';
+	const defaultAvatar = `https://avatars.io/twitter/${note.repositoryFullName}`;
+	const avatarSrc =
+		note.commentAvatar || note.repositoryOwnerAvatar || defaultAvatar;
+	const isClosed =
+		note.api.subject &&
+		note.api.subject.state &&
+		note.api.subject.state === 'closed';
 	const isMerged = note.api.subject && note.api.subject.merged;
 	const iconType = isMerged || isClosed ? 'checkmark-circle' : 'chat';
 	const iconText = isMerged || isClosed ? 'closed' : 'open';
 	const iconClasses = [
 		'notification__type',
-		...( isClosed && ! isMerged ? [ 'notification__type--closed' ] : [] ),
-		...( isMerged ? [ 'notification__type--merged' ] : [] ),
+		...(isClosed && !isMerged ? ['notification__type--closed'] : []),
+		...(isMerged ? ['notification__type--merged'] : []),
 	];
 
-	return el( 'div', { className: 'notification' + noteClass, onClick },
-		el( 'div', { className: iconClasses.join( ' ' ) }, el( Gridicon, { icon: iconType } ), el( 'span', { className: 'notification__type-text' }, iconText ) ),
-		el( 'div', { className: 'notification__image' },
-			isUnread ? el( 'span', { className: 'notification__new-dot' } ) : null,
-			el( EnsuredImage, { src: avatarSrc } ) ),
-		el( 'div', { className: 'notification__body' },
-			el( 'div', { className: 'notification__repo' }, note.repositoryFullName ),
-			el( 'div', { className: 'notification__title' }, note.title ),
-			el( 'div', {}, el( 'span', { className: 'notification__time' }, timeString ), isUnread ? el( MarkReadButton, { note, token, markRead } ) : el( MarkUnreadButton, { note, markUnread } ) )
-		)
+	return (
+		<div className={'notification' + noteClass} onClick={onClick}>
+			<div className={iconClasses.join(' ')}>
+				<Gridicon icon={iconType} />
+				<span className="notification__type--text">{iconText}</span>
+			</div>
+			<div className="notification__image">
+				{isUnread && <span className="notification__new-dot" />}
+				<EnsuredImage src={avatarSrc} />
+			</div>
+			<div className="notification__body">
+				<div className="notification__repo">{note.repositoryFullName}</div>
+				<div className="notification__title">{note.title}</div>
+				<div className="notification__footer">
+					<span className="notification__time">{timeString}</span>
+					{isUnread ? (
+						<MarkReadButton note={note} token={token} markRead={markRead} />
+					) : (
+						<MarkUnreadButton note={note} markUnread={markUnread} />
+					)}
+				</div>
+			</div>
+		</div>
 	);
 }
 
-function MarkReadButton( { note, token, markRead } ) {
+function MarkReadButton({ note, token, markRead }) {
 	const onClick = event => {
-		debug( 'clicked to mark notification as read', note );
+		debug('clicked to mark notification as read', note);
 		event.preventDefault();
 		event.stopPropagation();
-		markRead( token, note );
+		markRead(token, note);
 	};
-	return el( 'a', { className: 'notification__mark-read', href: '#', title: 'Mark as read', onClick }, 'mark read' );
+	return (
+		<a
+			className="notification__mark-read"
+			href="#"
+			title="Mark as read"
+			onClick={onClick}
+		>
+			mark read
+		</a>
+	);
 }
 
-function MarkUnreadButton( { note, markUnread } ) {
+function MarkUnreadButton({ note, markUnread }) {
 	const onClick = event => {
-		debug( 'clicked to mark notification as unread', note );
+		debug('clicked to mark notification as unread', note);
 		event.preventDefault();
 		event.stopPropagation();
-		markUnread( note );
+		markUnread(note);
 	};
-	return el( 'a', { className: 'notification__mark-unread', href: '#', title: 'Mark as unread', onClick }, 'mark unread' );
+	return (
+		<a
+			className="notification__mark-unread"
+			href="#"
+			title="Mark as unread"
+			onClick={onClick}
+		>
+			mark unread
+		</a>
+	);
 }
