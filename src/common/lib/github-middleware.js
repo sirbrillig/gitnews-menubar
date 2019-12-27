8c617ab411e76fdb7e8cadbbf7d4f0fc90de76df
Payton Swick <payton@foolord.com>
2019-12-27 12:57:41 -0500

    Disable markNoteRead and writeState in demo mode

--- src/common/lib/github-middleware.js
+++ src/common/lib/github-middleware.js
@@ -1,15 +1,19 @@
-const { markNotificationRead } = require( 'gitnews' );
+require('dotenv').config();
 
-const githubMiddleware = store => next => action => { // eslint-disable-line no-unused-vars
-	switch ( action.type ) {
+const { markNotificationRead } = require('gitnews');
+const isDemoMode = process.env.GITNEWS_DEMO_MODE ? true : false;
+
+// eslint-disable-next-line no-unused-vars
+const githubMiddleware = store => next => action => {
+	switch (action.type) {
 		case 'MARK_NOTE_READ':
-			markNoteRead( action.token, action.note );
+			isDemoMode || markNoteRead(action.token, action.note);
 	}
-	next( action );
+	next(action);
 };
 
-function markNoteRead( token, note ) {
-	markNotificationRead( token, note );
+function markNoteRead(token, note) {
+	markNotificationRead(token, note);
 }
 
 module.exports = {
--- src/common/lib/serialize-middleware.js
+++ src/common/lib/serialize-middleware.js
@@ -1,28 +1,30 @@
 /* globals window */
+require('dotenv').config();
+
+const isDemoMode = process.env.GITNEWS_DEMO_MODE ? true : false;
 
 const serializeMiddleware = store => next => action => {
-	switch ( action.type ) {
+	switch (action.type) {
 		case 'MARK_NOTE_UNREAD':
 		case 'MARK_NOTE_READ':
 		case 'MARK_ALL_NOTES_SEEN':
-			// TODO: this needs to happen AFTER the reducer changes the state
-			writeState( { notes: store.getState().notes } );
+			isDemoMode || writeState({ notes: store.getState().notes });
 	}
-	next( action );
+	next(action);
 };
 
-function writeState( state ) {
-	window.localStorage.setItem( 'gitnews-state', JSON.stringify( state ) );
+function writeState(state) {
+	window.localStorage.setItem('gitnews-state', JSON.stringify(state));
 }
 
 function getState() {
-	const stateSting = window.localStorage.getItem( 'gitnews-state' );
-	if ( ! stateSting ) {
+	const stateSting = window.localStorage.getItem('gitnews-state');
+	if (!stateSting) {
 		return {};
 	}
 	try {
-		return JSON.parse( stateSting );
-	} catch ( error ) {
+		return JSON.parse(stateSting);
+	} catch (error) {
 		return {};
 	}
 }
