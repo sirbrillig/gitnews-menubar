1fd9f0ac3eb0cdba27bfedc0e0f49289520fb2b3
Payton Swick <payton@foolord.com>
2020-03-17 23:27:45 -0400

    Run prettier on lib/updates

--- src/common/lib/updates.js
+++ src/common/lib/updates.js
@@ -1,21 +1,23 @@
-const getLatestRelease = require( 'get-latest-release' );
+const getLatestRelease = require('get-latest-release');
 
-function checkForNewVersion( { version, semver } ) {
-	return getLatestRelease( {
+function checkForNewVersion({ version, semver }) {
+	return getLatestRelease({
 		owner: 'sirbrillig',
 		repo: 'gitnews-menubar',
 		ext: 'dmg',
-	} )
-		.then( getVersionComparator( { version, semver } ) );
+	}).then(getVersionComparator({ version, semver }));
 }
 
-function getVersionComparator( { version, semver } ) {
-	return function isVersionNewer( response ) {
+function getVersionComparator({ version, semver }) {
+	return function isVersionNewer(response) {
 		try {
 			const newVersion = response.version;
 			const oldVersion = version;
-			console.log( `Comparing current version ${ oldVersion } and latest version ${ newVersion }` );
-			const updateAvailable = semver.gt( newVersion, oldVersion ) && ! semver.prerelease( newVersion );
+			console.log(
+				`Comparing current version ${oldVersion} and latest version ${newVersion}`
+			);
+			const updateAvailable =
+				semver.gt(newVersion, oldVersion) && !semver.prerelease(newVersion);
 			return {
 				updateAvailable,
 				newVersion,
@@ -23,39 +25,48 @@ function getVersionComparator( { version, semver } ) {
 				newVersionUrl: response.url,
 				newVersionDownloadUrl: response.asset && response.asset.download_url,
 			};
-		} catch ( err ) {
-			return Promise.reject( err );
+		} catch (err) {
+			return Promise.reject(err);
 		}
 	};
 }
 
-function checkForUpdates( { showCurrentVersion, version, semver, dialog, openUrl, download } ) {
-	checkForNewVersion( { version, semver } )
-		.then( response => {
-			if ( ! response.updateAvailable ) {
-				showCurrentVersion && dialog.showMessageBox( {
-					type: 'info',
-					message: `You have the latest version of Gitnews, ${ response.oldVersion }!`,
-				} );
+function checkForUpdates({
+	showCurrentVersion,
+	version,
+	semver,
+	dialog,
+	openUrl,
+	download,
+}) {
+	checkForNewVersion({ version, semver })
+		.then(response => {
+			if (!response.updateAvailable) {
+				showCurrentVersion &&
+					dialog.showMessageBox({
+						type: 'info',
+						message: `You have the latest version of Gitnews, ${response.oldVersion}!`,
+					});
 				return;
 			}
-			const confirm = dialog.showMessageBox( {
+			const confirm = dialog.showMessageBox({
 				type: 'question',
-				message: 'A new version ' + response.newVersion + ' of Gitnews is available.',
+				message:
+					'A new version ' + response.newVersion + ' of Gitnews is available.',
 				detail: 'Do you want to download it now?',
-				buttons: [ 'Yes', 'No' ],
-			} );
-			if ( confirm === 0 ) {
-				if ( download && response.newVersionDownloadUrl ) {
-					download( response.newVersionDownloadUrl );
+				buttons: ['Yes', 'No'],
+			});
+			if (confirm === 0) {
+				if (download && response.newVersionDownloadUrl) {
+					download(response.newVersionDownloadUrl);
 				} else {
-					openUrl( response.newVersionUrl );
+					openUrl(response.newVersionUrl);
 				}
 			}
-		} )
-		.catch( err => {
-			console.error( 'Error while checking for updates:', err );
-		} );
+		})
+		.catch(err => {
+			console.error('Error while checking for updates:', err);
+		});
 }
 
 module.exports = {
