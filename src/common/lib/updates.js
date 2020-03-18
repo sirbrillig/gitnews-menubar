bc4afc39ffbc1f7f789dc1d92bf380af804e7eff
Payton Swick <payton@foolord.com>
2020-03-17 23:39:27 -0400

    Create custom getLatestRelease

--- src/common/lib/updates.js
+++ src/common/lib/updates.js
@@ -1,33 +1,24 @@
-const getLatestRelease = require('get-latest-release');
-
-function checkForNewVersion({ version, semver }) {
-	return getLatestRelease({
-		owner: 'sirbrillig',
-		repo: 'gitnews-menubar',
-		ext: 'dmg',
-	}).then(getVersionComparator({ version, semver }));
+async function checkForNewVersion({ version, semver }) {
+	return getLatestRelease().then(getVersionComparator({ version, semver }));
 }
 
 function getVersionComparator({ version, semver }) {
-	return function isVersionNewer(response) {
-		try {
-			const newVersion = response.version;
-			const oldVersion = version;
-			console.log(
-				`Comparing current version ${oldVersion} and latest version ${newVersion}`
-			);
-			const updateAvailable =
-				semver.gt(newVersion, oldVersion) && !semver.prerelease(newVersion);
-			return {
-				updateAvailable,
-				newVersion,
-				oldVersion,
-				newVersionUrl: response.url,
-				newVersionDownloadUrl: response.asset && response.asset.download_url,
-			};
-		} catch (err) {
-			return Promise.reject(err);
-		}
+	return async function isVersionNewer(latestVersionData) {
+		const newVersion = latestVersionData.version;
+		const oldVersion = version;
+		console.log(
+			`Comparing current version ${oldVersion} and latest version ${newVersion}`
+		);
+		const updateAvailable =
+			semver.gt(newVersion, oldVersion) && !semver.prerelease(newVersion);
+		return {
+			updateAvailable,
+			newVersion,
+			oldVersion,
+			newVersionUrl: latestVersionData.url,
+			newVersionDownloadUrl:
+				latestVersionData.asset && latestVersionData.asset.download_url,
+		};
 	};
 }
 
@@ -67,6 +58,25 @@ function checkForUpdates({
 		.catch(err => {
 			console.error('Error while checking for updates:', err);
 		});
+}
+
+async function getLatestRelease() {
+	const url =
+		'https://api.github.com/repos/sirbrillig/gitnews-menubar/releases/latest';
+	const response = await fetch(url);
+	const responseData = response.json();
+	const asset = response.assets
+		? response.assets.find(asset => asset.name.includes('dmg'))
+		: null;
+	if (!asset) {
+		throw new Error("Couldn't find latest release.");
+	}
+	const latestRelease = {
+		version: responseData.tag_name,
+		url: responseData.html_url,
+		download_url: asset.browser_download_url,
+	};
+	return latestRelease;
 }
 
 module.exports = {
