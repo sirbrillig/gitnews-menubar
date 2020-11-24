3f31b85e7f82d6bdb83fbea3a94856e87ac67d32
Payton Swick <payton@foolord.com>
2020-11-24 17:41:49 -0500

    Remove Vanisher

--- src/renderer/components/vanisher.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Vanisher({ isVisible, children }) {
-	const className = isVisible
-		? 'vanisher vanisher--visible'
-		: 'vanisher vanisher--invisible';
-	return <div className={className}>{children}</div>;
-}
