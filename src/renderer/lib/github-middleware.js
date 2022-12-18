c9669b39ab254ee00df942e3d6a0a2e086ecbd6b
Payton Swick <payton@foolord.com>
2022-12-18 12:46:54 -0500

    Fix markNoteRead

--- src/renderer/lib/github-middleware.js
+++ src/renderer/lib/github-middleware.js
@@ -1,8 +1,13 @@
 // require('dotenv').config();
 
-// const { markNotificationRead } = require('gitnews');
+const { createNoteMarkRead } = require('gitnews');
 
 export function createGitHubMiddleware(isDemoMode) {
+	const markNotificationRead = createNoteMarkRead({
+		fetch: (url, options) => fetch(url, options),
+		log: (message) => console.log("Gitnews: " + message),
+	});
+
 	// eslint-disable-next-line no-unused-vars,no-undef
 	return store => next => action => {
 		switch (action.type) {
@@ -11,10 +16,8 @@ export function createGitHubMiddleware(isDemoMode) {
 		}
 		next(action);
 	};
-}
 
-function markNoteRead(token, note) {
-	note;
-	// TODO: fix marking notifications read
-	// markNotificationRead(token, note);
+	function markNoteRead(token, note) {
+		markNotificationRead(token, note);
+	}
 }
