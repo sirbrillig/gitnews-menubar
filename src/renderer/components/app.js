913ffb6010b1307fcb3c384d06af829a0c7c3b64
Payton Swick <payton@foolord.com>
2022-12-19 21:25:42 -0500

    Remove old AutoLaunch code

--- src/renderer/components/app.js
+++ src/renderer/components/app.js
@@ -1,4 +1,3 @@
-// import AutoLaunch from 'auto-launch';
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -54,15 +53,6 @@ class App extends React.Component {
 	componentDidMount() {
 		debug('App mounted');
 		this.fetcher.begin();
-		// const autoLauncher = new AutoLaunch({ name: 'Gitnews' });
-		// autoLauncher
-		// 	.isEnabled()
-		// 	.then(isEnabled => {
-		// 		this.props.changeAutoLoad(isEnabled);
-		// 	})
-		// 	.catch(function(err) {
-		// 		console.error('failed to fetch autoload', err); // eslint-disable-line no-console
-		// 	});
 	}
 
 	componentWillUnmount() {
