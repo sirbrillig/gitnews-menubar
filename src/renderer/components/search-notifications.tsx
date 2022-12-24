77e6fcbed4cfb4d45bf73f5d7de21ef79a5deeac
Payton Swick <payton@foolord.com>
2022-12-23 23:10:20 -0500

    Convert search-notifications to TS

--- src/renderer/components/search-notifications.tsx
+++ src/renderer/components/search-notifications.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Gridicon from 'gridicons';
 
-export default function SearchNotifications({ searchValue, setSearchTo }) {
+export default function SearchNotifications({
+	searchValue,
+	setSearchTo,
+}: {
+	searchValue: string;
+	setSearchTo: (value: string) => void;
+}) {
 	return <SearchField setSearchTo={setSearchTo} searchValue={searchValue} />;
 }
 
-function SearchField({ setSearchTo, searchValue }) {
+function SearchField({
+	setSearchTo,
+	searchValue,
+}: {
+	setSearchTo: (value: string) => void;
+	searchValue: string;
+}) {
 	return (
 		<div className="notifications__search-area">
 			<Gridicon icon="search" size={36} className="search-icon" />
