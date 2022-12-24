4d01a102666de0b9a045fd5e247ede1787d7dee5
Payton Swick <payton@foolord.com>
2022-12-23 23:54:37 -0500

    Convert lib/updates to TS

--- src/renderer/lib/updates.ts
+++ src/renderer/lib/updates.ts
@@ -1,11 +1,22 @@
 import semver from 'semver';
 import React from 'react';
 
+interface LatestRelease {
+	name: string;
+	browser_download_url: string;
+}
+
+interface LatestReleaseResponse {
+	assets?: LatestRelease[];
+	tag_name: string;
+	html_url: string;
+}
+
 async function getLatestRelease() {
 	const url =
 		'https://api.github.com/repos/sirbrillig/gitnews-menubar/releases/latest';
 	const response = await fetch(url);
-	const responseData = await response.json();
+	const responseData: LatestReleaseResponse = await response.json();
 	const asset = responseData.assets
 		? responseData.assets.find(asset => asset.name.includes('dmg'))
 		: null;
@@ -25,7 +36,8 @@ async function getInfoIfUpdateAvailable() {
 	const oldVersion = await window.electronApi.getVersion();
 	const latestVersionInfo = await getLatestRelease();
 	const newVersion = latestVersionInfo.version;
-	const isUpdateAvailable = semver.gt(newVersion, oldVersion) && !semver.prerelease(newVersion);
+	const isUpdateAvailable =
+		semver.gt(newVersion, oldVersion) && !semver.prerelease(newVersion);
 	if (isUpdateAvailable) {
 		return latestVersionInfo;
 	}
@@ -33,14 +45,22 @@ async function getInfoIfUpdateAvailable() {
 }
 
 export function useGetGitnewsUpdate() {
-	const [updateData, setUpdateData] = React.useState({isUpdateAvailable: false, updateUrl: undefined, updatedVersion: undefined});
+	const [updateData, setUpdateData] = React.useState({
+		isUpdateAvailable: false,
+		updateUrl: undefined,
+		updatedVersion: undefined,
+	});
 	React.useEffect(() => {
-		getInfoIfUpdateAvailable().then((latestVersionInfo) => {
-			if (! latestVersionInfo) {
+		getInfoIfUpdateAvailable().then(latestVersionInfo => {
+			if (!latestVersionInfo) {
 				return;
 			}
-			setUpdateData({isUpdateAvailable: true, updateUrl: latestVersionInfo.url, updatedVersion: latestVersionInfo.version})
-		})
+			setUpdateData({
+				isUpdateAvailable: true,
+				updateUrl: latestVersionInfo.url,
+				updatedVersion: latestVersionInfo.version,
+			});
+		});
 	});
 	return updateData;
 }
