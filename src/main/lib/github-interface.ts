852aa711ad12d53beb9e75dc366a69864cdb1bc8
Payton Swick <payton@foolord.com>
2024-09-08 17:07:10 -0400

    Add comment avatar to each note

--- src/main/lib/github-interface.ts
+++ src/main/lib/github-interface.ts
@@ -9,12 +9,32 @@ export async function fetchNotificationsForAccount(
 		baseUrl: account.serverUrl,
 		userAgent: 'gitnews-menubar',
 	});
-	const notifications =
+	const notificationsResponse =
 		await octokit.rest.activity.listNotificationsForAuthenticatedUser({
 			all: false,
 		});
-	return notifications.data.map((notification) => {
-		return {
+
+	const notes: Note[] = [];
+
+	for (const notification of notificationsResponse.data) {
+		let commentAvatar;
+
+		if (notification.subject.latest_comment_url) {
+			try {
+				const commentUrl = new URL(notification.subject.latest_comment_url);
+				const commentUrlPath = commentUrl.pathname;
+				const comment = await octokit.request(`GET ${commentUrlPath}`, {});
+				commentAvatar = comment.data.user.avatar_url;
+			} catch (error) {
+				console.error(
+					`Failed to fetch comment for ${notification.subject.latest_comment_url}`
+				);
+				// This might fail but it's not that important so we will ignore it if
+				// that happens.
+			}
+		}
+
+		notes.push({
 			id: notification.id,
 			title: notification.subject.title,
 			unread: notification.unread,
@@ -24,12 +44,14 @@ export async function fetchNotificationsForAccount(
 			repositoryName: notification.repository.name,
 			type: notification.subject.type,
 			subjectUrl: notification.subject.url,
-			commentAvatar: notification.repository.owner.avatar_url, // FIXME I think this comes from the commentUrl
+			commentAvatar: commentAvatar ?? notification.repository.owner.avatar_url,
 			repositoryOwnerAvatar: notification.repository.owner.avatar_url,
 			api: {
 				subject: { state: undefined, merged: undefined }, // FIXME I think this comes from the subjectUrl
 				notification: { reason: notification.reason as NoteReason },
 			},
-		};
-	});
+		});
+	}
+
+	return notes;
 }
