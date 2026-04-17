e5bf2024ea2944fd6393816e20f437ed08b78fca
Payton Swick <payton@foolord.com>
2026-04-17 16:48:51 -0400

    Make sure we treat a GHE connection failure as normal failure

--- src/renderer/lib/gitnews-fetcher.ts
+++ src/renderer/lib/gitnews-fetcher.ts
@@ -364,6 +364,7 @@ function dispatchAccountFetchError(
 		const message = `GitHub Enterprise server for "${account.name}" is temporarily unavailable (server may be starting up)`;
 		debug(message);
 		window.electronApi.logMessage(message, 'warn');
+		dispatch(addConnectionError(message));
 		return;
 	}
 	const message = `Error fetching notifications for "${account.name}": ${getErrorMessage(err as UnknownFetchError)}`;
