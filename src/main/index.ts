5268913388aefef38cac56d3c489c932b1b8658f
Payton Swick <payton@foolord.com>
2026-05-26 13:08:13 -0400

    Restore original 430px app width

--- src/main/index.ts
+++ src/main/index.ts
@@ -58,7 +58,7 @@ const bar = menubar({
 	index: MAIN_WINDOW_WEBPACK_ENTRY,
 	icon: getIconForState('loading'),
 	browserWindow: {
-		width: 440,
+		width: 430,
 		height: 600,
 		webPreferences: {
 			nodeIntegration: true,
